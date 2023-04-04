e05cbe07cde63ec27c9a34ea8e152f5d66390b1c
AliceLambertRutayisire <90448938+AliceLambertRutayisire@users.noreply.github.com>
2023-04-04 19:23:11 +0200

    Update splash.dart

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-
 import 'main.dart';
 
 class Logo extends StatefulWidget {
@@ -11,21 +9,26 @@ class Logo extends StatefulWidget {
 
 class _LogoState extends State<Logo> {
   @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 1), (() {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => MyHomePage()));
-    }));
-  }
+  // void initState() {
+  //   super.initState();
+  //   Timer(Duration(seconds: 1), (() {
+  //     Navigator.pushReplacement(
+  //         context, MaterialPageRoute(builder: (context) => MyHomePage()));
+  //   }));
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(201, 199, 126, 1),
-      body: Center(
-          child: Image.asset("assets/images/foodease.png",
-              width: 346, height: 376)),
-    );
+      body: 
+           AnimatedSplashScreen(splash: Image.asset("assets/images/foodease.png",  width: 346, height: 376), duration: 3000,
+            splashTransition: SplashTransition.fadeTransition,
+            backgroundColor: Color.fromRGBO(201, 199, 126, 1),
+            nextScreen: MyHomePage(),
+            centered: true,
+        
+         ),
+       );
   }
 }
